5d2d329f58214fd393441f516ae0ec8ab5b08c1e
castro <112527829+cxstro06@users.noreply.github.com>
2023-03-09 08:53:20 -0300

    Update perso-final.js

---
+++ perso-final.js
@@ -0,0 +1,39 @@
+function colorRed() {
+    document.getElementById ('textName').style.color = 'red'
+}
+
+function colorOrange() {
+    document.getElementById ('textName').style.color = 'orange'
+}
+
+function colorYellow() {
+    document.getElementById ('textName').style.color = 'yellow'
+}
+
+function colorLightgreen() {
+    document.getElementById ('textName').style.color = 'greenyellow'
+}
+
+function colorGreen() {
+    document.getElementById ('textName').style.color = 'green'
+}
+
+function colorlightblue() {
+    document.getElementById ('textName').style.color = 'lightskyblue'
+}
+
+function colorBlue() {
+    document.getElementById ('textName').style.color = 'blue'
+}
+
+function colorPurple() {
+    document.getElementById ('textName').style.color = 'blueviolet'
+}
+
+function colorPink() {
+    document.getElementById ('textName').style.color = 'deeppink'
+}
+
+function colorBlack() {
+    document.getElementById ('textName').style.color = 'black'
+}
